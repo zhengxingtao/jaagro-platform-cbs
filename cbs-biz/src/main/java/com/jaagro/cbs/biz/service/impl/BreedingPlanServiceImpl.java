3fac44e2735568df2f761519ce2e6d1c8a58e4bf
gaoxin <1506645910@qq.com>
2019-02-22 15:37:43 +0800

    单号自增

--- cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/BreedingPlanServiceImpl.java
+++ cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/BreedingPlanServiceImpl.java
@@ -2,14 +2,19 @@ package com.jaagro.cbs.biz.service.impl;
 
 import com.jaagro.cbs.api.dto.plan.CreateBreedingPlanDto;
 import com.jaagro.cbs.api.service.BreedingPlanService;
+import com.jaagro.cbs.biz.mapper.BatchPlantCoopMapperExt;
 import com.jaagro.cbs.biz.mapper.BreedingPlanMapperExt;
+import com.jaagro.cbs.biz.model.BatchPlantCoop;
 import com.jaagro.cbs.biz.model.BreedingPlan;
 import com.jaagro.cbs.biz.utils.SequenceCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 
 /**
@@ -25,6 +30,8 @@ public class BreedingPlanServiceImpl implements BreedingPlanService {
     private SequenceCodeUtils sequenceCodeUtils;
     @Autowired
     private BreedingPlanMapperExt breedingPlanMapper;
+    @Autowired
+    private BatchPlantCoopMapperExt batchPlantCoopMapper;
 
     /**
      * 创建养殖计划
@@ -32,6 +39,7 @@ public class BreedingPlanServiceImpl implements BreedingPlanService {
      * @param dto
      */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void createBreedingPlan(CreateBreedingPlanDto dto) {
         String batchNo = sequenceCodeUtils.genSeqCode("AT");
         BreedingPlan breedingPlan = new BreedingPlan();
@@ -44,8 +52,15 @@ public class BreedingPlanServiceImpl implements BreedingPlanService {
         //插入养殖计划
         breedingPlanMapper.insertSelective(breedingPlan);
         //插入养殖关联表
-        if(CollectionUtils.isEmpty(dto.getPlantsId())){
-
+        if (CollectionUtils.isEmpty(dto.getPlantsId())) {
+            List<Integer> plantsIds = dto.getPlantsId();
+            for (Integer plantsId : plantsIds) {
+                BatchPlantCoop batchPlantCoop = new BatchPlantCoop();
+                batchPlantCoop
+                        .setBatchNo(batchNo)
+                        .setCreateUserId(null)
+                        .setPlantId(plantsId);
+            }
         }
     }
 }
