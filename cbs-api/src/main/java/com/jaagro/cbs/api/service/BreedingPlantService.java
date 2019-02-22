12a37b47723b95bdf02b36abb944eada5e13d160
baiyiran <1169172027@qq.com>
2019-02-22 10:25:31 +0800

    养殖场基础api--1

--- cbs-api/src/main/java/com/jaagro/cbs/api/service/BreedingPlantService.java
+++ cbs-api/src/main/java/com/jaagro/cbs/api/service/BreedingPlantService.java
@@ -1,5 +1,12 @@
 package com.jaagro.cbs.api.service;
 
+import com.jaagro.cbs.api.dto.breedingPlant.CreatePlantDto;
+import com.jaagro.cbs.api.dto.breedingPlant.ReturnPlantDto;
+import com.jaagro.cbs.api.dto.breedingPlant.UpdatePlantDto;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * 养殖基本资料server，包含养殖场、图片、鸡舍等api都在这个里
  *
@@ -8,4 +15,35 @@ package com.jaagro.cbs.api.service;
  */
 public interface BreedingPlantService {
 
+    /**
+     * 新增
+     *
+     * @param plantDto
+     * @return
+     */
+    Map<String, Object> createPlant(CreatePlantDto plantDto);
+
+    /**
+     * 修改
+     *
+     * @param plantDto
+     * @return
+     */
+    Map<String, Object> updatePlant(UpdatePlantDto plantDto);
+
+    /**
+     * 通过养殖id获取养殖场详情
+     *
+     * @param id
+     * @return
+     */
+    ReturnPlantDto getPlantDetailsById(Integer id);
+
+    /**
+     * 通过养殖户id获取养殖场列表
+     *
+     * @param customerId
+     * @return
+     */
+    List<ReturnPlantDto> listPlantByCustomerId(Integer customerId);
 }
---
+++ cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/BreedingPlantServiceImpl.java
@@ -0,0 +1,68 @@
+package com.jaagro.cbs.biz.service.impl;
+
+import com.jaagro.cbs.api.dto.breedingPlant.CreatePlantDto;
+import com.jaagro.cbs.api.dto.breedingPlant.ReturnPlantDto;
+import com.jaagro.cbs.api.dto.breedingPlant.UpdatePlantDto;
+import com.jaagro.cbs.api.service.BreedingPlantService;
+import com.jaagro.cbs.biz.mapper.PlantMapperExt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author baiyiran
+ * @Date 2019/2/22
+ */
+@Service
+public class BreedingPlantServiceImpl implements BreedingPlantService {
+
+    @Autowired
+    private PlantMapperExt plantMapper;
+
+
+    /**
+     * 新增
+     *
+     * @param plantDto
+     * @return
+     */
+    @Override
+    public Map<String, Object> createPlant(CreatePlantDto plantDto) {
+        return null;
+    }
+
+    /**
+     * 修改
+     *
+     * @param plantDto
+     * @return
+     */
+    @Override
+    public Map<String, Object> updatePlant(UpdatePlantDto plantDto) {
+        return null;
+    }
+
+    /**
+     * 通过养殖id获取养殖场详情
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public ReturnPlantDto getPlantDetailsById(Integer id) {
+        return null;
+    }
+
+    /**
+     * 通过养殖户id获取养殖场列表
+     *
+     * @param customerId
+     * @return
+     */
+    @Override
+    public List<ReturnPlantDto> listPlantByCustomerId(Integer customerId) {
+        return null;
+    }
+}
