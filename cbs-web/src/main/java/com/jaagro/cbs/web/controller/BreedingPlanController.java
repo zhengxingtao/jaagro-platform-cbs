9dcb670ea33bb69d99cb275c4e051423f136c836
gaoxin <1506645910@qq.com>
2019-02-22 11:34:53 +0800

    单号自增

---
+++ cbs-web/src/main/java/com/jaagro/cbs/web/controller/BreedingPlanController.java
@@ -0,0 +1,30 @@
+package com.jaagro.cbs.web.controller;
+
+import com.jaagro.cbs.api.dto.plan.CreateBreedingPlanDto;
+import com.jaagro.cbs.api.service.BreedingPlanService;
+import com.jaagro.utils.BaseResponse;
+import com.jaagro.utils.ResponseStatusCode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author @Gao.
+ */
+@RestController
+@Api(description = "养殖计划管理", produces = MediaType.APPLICATION_JSON_VALUE)
+public class BreedingPlanController {
+    @Autowired
+    private BreedingPlanService breedingPlanService;
+
+    @PostMapping("/createBreedingPlan")
+    @ApiOperation("创建养殖计划")
+    public BaseResponse createBreedingPlan(@RequestBody CreateBreedingPlanDto dto) {
+        breedingPlanService.createBreedingPlan(dto);
+        return BaseResponse.successInstance(ResponseStatusCode.OPERATION_SUCCESS);
+    }
+}
