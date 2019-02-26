b2252871973ff78473ff9c3aa5fb357a3e9d28a5
zhengxingtao <zheng_xigntao@126.com>
2019-02-26 11:23:38 +0800

    优化utils代码

--- cbs-biz/src/main/java/com/jaagro/cbs/biz/utils/SequenceCodeUtils.java
+++ cbs-biz/src/main/java/com/jaagro/cbs/biz/utils/SequenceCodeUtils.java
@@ -57,11 +57,10 @@ public class SequenceCodeUtils {
     private String fixLengthString(int strLength) {
         Random rm = new Random();
         // 获得随机数
-        double pross = (1 + rm.nextDouble()) * Math.pow(10, strLength);
+        double random = (1 + rm.nextDouble()) * Math.pow(10, strLength);
         // 将获得的获得随机数转化为字符串
-        String fixLenthString = String.valueOf(pross);
+        String fixLengthString = String.valueOf(random);
         // 返回固定的长度的随机数
-        return fixLenthString.substring(2, strLength + 1);
+        return fixLengthString.substring(2, strLength + 1);
     }
-
 }
