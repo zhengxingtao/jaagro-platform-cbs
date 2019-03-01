ac260dae20c1082a7b628a93faf83dc204849b3c
gaoxin <1506645910@qq.com>
2019-03-01 10:52:52 +0800

    Merge branch 'test' of https://github.com/zhengxingtao/jaagro-platform-cbs into gx
    # Conflicts:
    #	cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/BreedingPlanServiceImpl.java

---
+++ cbs-api/src/main/java/com/jaagro/cbs/api/dto/plan/CalculatePurchaseOrderParamDto.java
@@ -0,0 +1,21 @@
+package com.jaagro.cbs.api.dto.plan;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @description: 用于结算采购订单中间对象
+ * @author: @Gao.
+ * @create: 2019-03-01 10:00
+ **/
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class CalculatePurchaseOrderParamDto implements Serializable {
+
+}
