24c0ea3aebb803bade197707d7f62a1bef404525
wanghq <gavinwang0224@aliyun.com>
2019-03-11 10:28:16 +0800

    养殖大脑-饲料订单API

--- cbs-api/src/main/java/com/jaagro/cbs/api/service/BreedingBrainService.java
+++ cbs-api/src/main/java/com/jaagro/cbs/api/service/BreedingBrainService.java
@@ -2,7 +2,6 @@ package com.jaagro.cbs.api.service;
 
 import com.jaagro.cbs.api.model.PurchaseOrder;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -20,20 +19,26 @@ public interface BreedingBrainService {
     List<PurchaseOrder> calculatePhaseOneFoodWeightById(Integer planId);
 
     /**
-     * 根据养殖计划Id生成养殖第二阶段（15->19天）的饲料订单、20->28天的饲料订单
+     * 根据养殖计划Id生成养殖第二阶段（15->19天）的饲料订单
      *
      * @param planId
-     * @return 返回第二阶段需要采购的2个订单：15->19天的饲料订单、20->28天的饲料订单
+     * @return 返回第二阶段需要采购的2个订单：15->19天的饲料订单
      */
     List<PurchaseOrder> calculatePhaseTwoFoodWeightById(Integer planId);
 
+    /**
+     * 根据养殖计划Id生成养殖第三阶段20->28天的饲料订单
+     * @param planId
+     * @return
+     */
+    List<PurchaseOrder> calculatePhaseThreeFoodWeightById(Integer planId);
     /**
      * 根据养殖计划Id生成29->计划养殖天数的饲料订单
      * 饲料采购 - 第三次饲料配送
      * @param planId
      * @return 返回第三次饲料配送需要采购的饲料订单
      */
-    List<PurchaseOrder> calculatePhaseThreeFoodWeightById(Integer planId);
+    List<PurchaseOrder> calculatePhaseFourFoodWeightById(Integer planId);
 
 
 }
