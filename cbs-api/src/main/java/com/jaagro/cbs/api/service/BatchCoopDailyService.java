dbe16a7168f05e76872e3d4d47885a316964af03
baiyiran <1169172027@qq.com>
2019-03-16 17:31:32 +0800

    定时钟。。。

---
+++ cbs-api/src/main/java/com/jaagro/cbs/api/service/BatchCoopDailyService.java
@@ -0,0 +1,13 @@
+package com.jaagro.cbs.api.service;
+
+/**
+ * @author baiyiran
+ * @Date 2019/3/16
+ */
+public interface BatchCoopDailyService {
+
+    /**
+     * 鸡舍养殖每日汇总
+     */
+    void batchCoopDaily();
+}
