612fd9bef0fc5217de4fec6623c7636dab9a4cd6
gaoxin <1506645910@qq.com>
2019-03-18 13:54:45 +0800

    Merge branch 'test' of https://github.com/zhengxingtao/jaagro-platform-cbs into gx
    # Conflicts:
    #	cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/ProductServiceImpl.java

--- cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/ProductServiceImpl.java
+++ cbs-biz/src/main/java/com/jaagro/cbs/biz/service/impl/ProductServiceImpl.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
 
